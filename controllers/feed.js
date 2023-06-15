21ad0414fc2d42f63176927ae7edd80bea4b910d
drew-chidi <chidi.andrew@gmail.com>
2023-06-15 15:21:30 +0100

    clearing out post-user relations

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,6 +2,7 @@ const { validationResult } = require('express-validator');
 const fs = require('fs');
 const path = require('path');
 const Post = require('../models/post');
+const { userInfo } = require('os');
 
 // in createPosts, change the imageUrl const:
 
@@ -57,22 +58,31 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   const imageUrl = req.file.path.replace('\\', '/');
-
+  let creator;
   const post = new Post({
     title,
     content,
     imageUrl: imageUrl,
-    creator: { name: 'Andrew Chidi' },
+    creator: req.userId,
   });
 
   //   Create post in db
   post
     .save()
     .then((result) => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
       console.log(result);
       res.status(201).json({
         message: 'Post created successfully!',
-        post: result,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
@@ -129,6 +139,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!');
+        error.statusCode = 403;
+        throw error;
+      }
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -157,12 +172,22 @@ exports.deletePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized!');
+        error.statusCode = 403;
+        throw error;
+      }
       //   Check logged in user
       clearImage(post.imageUrl);
       return Post.findByIdAndRemoved(postId);
     })
     .then((result) => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+    })
+    .then((result) => {
       res.status(200).json({ message: 'Deleted post.' });
     })
     .catch((err) => {
